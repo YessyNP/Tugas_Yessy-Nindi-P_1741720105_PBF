6449f314cd23467f4aae608f8d76812c1ac229f6
YessyNP <yessynindi@gmail.com>
2020-04-15 13:09:57 +0700

    tugas joMenambahkan fungsi join with Googlein
    Menambahkan fungsi join with Google

--- yessynp-minggu11/src/Join.js
+++ yessynp-minggu11/src/Join.js
@@ -6,6 +6,7 @@ const Join = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setErrors] = useState("");
+  
   const Auth = useContext(AuthContext);
   const handleForm = (e) => {
     e.preventDefault();
@@ -20,9 +21,27 @@ const Join = () => {
       });
   };
 
+  const handleGoogleSignUp = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .then(() => {
+        firebase
+          .auth()
+          .signInWithPopup(provider)
+          .then((result) => {
+            console.log(result);
+            Auth.setLoggedIn(true);
+          })
+          .catch((e) => setErrors(e.message));
+      });
+  };
+
   return (
     <div>
       <h1>Join</h1>
+
       <form onSubmit={(e) => handleForm(e)}>
         <input
           value={email}
@@ -31,6 +50,7 @@ const Join = () => {
           type="email"
           placeholder="email"
         />
+
         <input
           onChange={(e) => setPassword(e.target.value)}
           name="password"
@@ -38,15 +58,23 @@ const Join = () => {
           type="password"
           placeholder="password"
         />
+
         <hr />
-        <button className="googleBtn" type="button">
+
+        <button
+          onClick={() => handleGoogleSignUp()}
+          className="googleBtn"
+          type="button"
+        >
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
             alt="logo"
           />
-          Join With Google
+          Join with Google
         </button>
-        <button type="submit">Login</button>
+
+        <button type="submit">Sign up</button>
+
         <span>{error}</span>
       </form>
     </div>
