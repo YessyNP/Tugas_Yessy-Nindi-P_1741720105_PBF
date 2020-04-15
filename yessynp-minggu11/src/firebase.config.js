45662ed28507a87c9a5dda87a4e9b7e874e6bfa5
YessyNP <yessynindi@gmail.com>
2020-04-15 11:40:43 +0700

    praktikum
    praktikum minggu 11

---
+++ yessynp-minggu11/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDEzkFCY397m3FLgBWDT88zqEfwVDAy0ZA",
+    authDomain: "pbf-minggu11.firebaseapp.com",
+    databaseURL: "https://pbf-minggu11.firebaseio.com",
+    projectId: "pbf-minggu11",
+    storageBucket: "pbf-minggu11.appspot.com",
+    messagingSenderId: "176205606389",
+    appId: "1:176205606389:web:1a43da5a8065e4c65304bc",
+    measurementId: "G-T3D7P015N9"
+  };
